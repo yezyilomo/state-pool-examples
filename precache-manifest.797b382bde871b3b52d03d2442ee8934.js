987fc50b460aeaef6476e15a7bb75cb57a2548cf
yezy <yezileliilomo@hotmail.com>
2022-03-03 01:40:26 +0200

    Updates

---
+++ precache-manifest.797b382bde871b3b52d03d2442ee8934.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7cfc49c808d88ab2acc00b17873fe91a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0f9f288ec242f0b1e927",
+    "url": "/static/css/main.9869ac35.chunk.css"
+  },
+  {
+    "revision": "fc195ab45550bb0b54af",
+    "url": "/static/js/2.0c2e4848.chunk.js"
+  },
+  {
+    "revision": "0f9f288ec242f0b1e927",
+    "url": "/static/js/main.b389f3e8.chunk.js"
+  },
+  {
+    "revision": "81acdf2dcdde2d4ecd3b",
+    "url": "/static/js/runtime-main.6a9b194c.js"
+  }
+]);
